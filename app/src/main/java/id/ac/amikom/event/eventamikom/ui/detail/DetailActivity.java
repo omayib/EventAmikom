68421b449e04423c20f5c66f20907f337734c89f
omayib <omayib@gmail.com>
2017-02-20 07:10:14 +0700

    preparing detail activity

---
+++ app/src/main/java/id/ac/amikom/event/eventamikom/ui/detail/DetailActivity.java
@@ -0,0 +1,15 @@
+package id.ac.amikom.event.eventamikom.ui.detail;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import id.ac.amikom.event.eventamikom.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
